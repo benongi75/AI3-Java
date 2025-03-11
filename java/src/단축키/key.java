ba7f5a8d3f16defc38eb07b667856a1419e6cc72
TJ-BU-706-P10 <TJ-BU-706-P10@DESKTOP-S7OBJAV>
2025-03-11 17:49:47 +0900

    2일차

---
+++ java/src/단축키/key.java
@@ -0,0 +1,55 @@
+package 단축키;
+
+// 주석      : 프로그램에 영향이 없는 메모 (코드 설명, 기록)
+// 주석      : ctrl + / , ctrl + shift + C
+// 한 줄 복사 : ctrl + alt + ↑ / ↓ (ㅁ한자)
+// 한 줄 삭제 : ctrl + D
+// 코드 이동  : alt + ↑ / ↓ 
+// 스크롤이동  : ctrl + ↑ / ↓
+// 새 파일 생성 : ctrl + N
+// 탭 종료     : ctrl + W
+// 전체 탭 종료 : ctrl + shift + W
+// 프로그램 실행 : ctrl + F11
+// 이름 바꾸기 : F2
+// 찾기 바꾸기 : ctrl + F
+// 코드찾기 : ctrl + H
+// 단어 찾기 : ctrl + K
+// 라인 찾기 : ctrl + L
+// 잘라내기 : ctrl + X
+// 모두 선택 : ctrl + A
+// 들여쓰기 : TAB
+// 내어쓰기 : ctrl + TAB
+// 전체 화면 : ctrl + M
+// 한 줄복사 : ctrl + alt + ↑ / ↓
+// 한 줄삭제 : ctrl + D
+// 파일 저장 : ctrl + S
+// 뒤로 가기 : ctrl + Z
+// 앞으로 가기 : ctrl + Y
+
+//public class 단축키 {
+	
+//	main
+//	sysout
+//	public static void mian(String[] args) {
+//		System.out.print("");
+		
+//	}
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * 
+ */
+public class key {
+
+}
